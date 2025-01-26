37d0774ecbb2171a0a8abd791c9bc519edd8f42f
cingihimut <cingihimutbanget@gmail.com>
2025-01-26 13:08:46 +0700

    user can deposit wbtc to vault

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -2,23 +2,28 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Vault} from "src/Vault.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract VaultTest is Test {
+
+    address public wbtc = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+
+    Vault public vault;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        vm.createSelectFork("https://eth-mainnet.g.alchemy.com/v2/jIQwSPn0l4YehffUHdlUcicsq1pMLEfu", 21699814);
+        vault = new Vault(wbtc);
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+    function test_deposit() public {
+        deal(wbtc, address(this), 1e8);
+        IERC20(wbtc).approve(address(vault), 1e8);
+        vault.deposit(1e8);
+        
+        uint256 vaultBalance = IERC20(wbtc).balanceOf(address(vault));
+        console.log("Vault balance: ", vaultBalance);
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        assertEq(vaultBalance, 1e8, "Vault balance should match the deposite amount");
     }
 }
