b489f635bfcd230c622dd46e96c50ca5b6f29d7d
cingihimut <cingihimutbanget@gmail.com>
2025-01-26 12:00:42 +0700

    make vault contract

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.13;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "";
 
+contract Vault is ERC20 {
 
-contract Vault {
-
+    address public immutable wbtc;
+    address public 
 }
