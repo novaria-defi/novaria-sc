d8056a2026b5a8ba7b2fd86537c9a63d6c73f8b6
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2025-02-03 21:50:39 +0700

    feat: order short done

--- src/interfaces/IGMX.sol
+++ src/interfaces/IGMX.sol
@@ -1,23 +1,18 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-struct PriceProps {
-    uint256 min;
-    uint256 max;
-}
-
 interface IExchangeRouter {
-    struct Addresses {
+    struct CreateOrderParamsAddresses {
         address receiver;
-        address callbackContract;
         address cancellationReceiver;
+        address callbackContract;
         address uiFeeReceiver;
         address market;
         address initialCollateralToken;
         address[] swapPath;
     }
 
-    struct Numbers {
+    struct CreateOrderParamsNumbers {
         uint256 sizeDeltaUsd;
         uint256 initialCollateralDeltaAmount;
         uint256 triggerPrice;
@@ -29,77 +24,21 @@ interface IExchangeRouter {
     }
 
     struct CreateOrderParams {
-        Addresses addresses;
-        Numbers numbers;
-        uint256 orderType;
-        uint256 decreasePositionSwapType;
+        CreateOrderParamsAddresses addresses;
+        CreateOrderParamsNumbers numbers;
+        uint8 orderType;
+        uint8 decreasePositionSwapType;
         bool isLong;
         bool shouldUnwrapNativeToken;
         bool autoCancel;
         bytes32 referralCode;
     }
 
-    function createOrder(
-        CreateOrderParams calldata params
-    ) external payable returns (bytes32);
+    function createOrder(CreateOrderParams memory params) external payable returns (bytes32);
 
-    function multicall(
-        bytes[] calldata data
-    ) external payable returns (bytes[] memory results);
+    function sendTokens(address token, address receiver, uint256 amount) external;
 
     function sendWnt(address receiver, uint256 amount) external payable;
-
-    function sendTokens(
-        address token,
-        address receiver,
-        uint256 amount
-    ) external payable;
-
-    function getPrice(address market) external view returns (uint256);
-}
-
-interface IOrderVault {
-    function executeOrder(bytes32 key, address keeper) external;
-
-    function cancelOrder(bytes32 key) external;
-}
-
-interface IReader {
-    function getMarketInfo(
-        IDataStore dataStore,
-        IMarketUtils.MarketPrices memory prices,
-        address market
-    ) external view returns (IReaderUtils.MarketInfo memory);
-
-    function getDataStore() external view returns (IDataStore);
-
-    function getPrices(
-        address market
-    ) external view returns (IMarketUtils.MarketPrices memory);
-}
-
-interface IDataStore {
-    function getUint(bytes32 key) external view returns (uint256);
-
-    function getAddress(bytes32 key) external view returns (address);
-
-    function getBytes32(bytes32 key) external view returns (bytes32);
-}
-
-interface IMarketUtils {
-    struct MarketPrices {
-        PriceProps indexTokenPrice;
-        PriceProps longTokenPrice;
-        PriceProps shortTokenPrice;
-    }
-}
-
-interface IReaderUtils {
-    struct MarketInfo {
-        uint256 indexTokenPrice;
-        uint256 longInterestUsd;
-        uint256 shortInterestUsd;
-    }
 }
 
 interface IReaderOrder {
@@ -138,10 +77,7 @@ interface IReaderOrder {
         Flags flags;
     }
 
-    function getOrder(
-        address dataStore,
-        bytes32 key
-    ) external view returns (Props memory);
+    function getOrder(address dataStore, bytes32 key) external view returns (Props memory);
 }
 
 interface IReaderPosition {
@@ -173,19 +109,5 @@ interface IReaderPosition {
         Flags flags;
     }
 
-    function getPosition(
-        address dataStore,
-        bytes32 key
-    ) external view returns (Props memory);
-}
-
-interface IRouter {
-    function swapTokensToETH(
-        address[] memory _path,
-        uint256 _amountIn,
-        uint256 _minOut,
-        address payable _receiver
-    ) external;
-
-    function multicall(bytes[] calldata data) external payable;
+    function getPosition(address dataStore, bytes32 key) external view returns (Props memory);
 }
